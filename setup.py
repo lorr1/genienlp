a364f3b12ed62bf246d8510241aad24153ab8a3c
mehrad <mehrad@stanford.edu>
2021-03-03 23:52:07 -0800

    Use bootleg 1.0.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
 
     install_requires=[
         'numpy>=1.14.5',
-        'torch~=1.7.1',
+        'torch~=1.8.0',
         'tqdm~=4.0',
         'tensorboardX==2.1.*',
         'pyrouge>=0.1.3',
@@ -64,7 +64,7 @@ setuptools.setup(
         'pathos==0.2.6',
         'marisa_trie_m==0.7.6',
         'kfserving>=0.5.0',
-        'bootleg @ git+https://github.com/Mehrad0711/bootleg@41aca545f2fee3d7af724183af37d1b28fb10748',
+        'bootleg @ git+https://github.com/Mehrad0711/bootleg@8efb6b35ce10d79f1c63a06bfece647857b17bca',
         # for calibration:
         'scikit-learn~=0.23',
         'dill~=0.3',
